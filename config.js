7152cd80598c5e00b56b001ffa33f391896c2d76
ninjajitrungpipat-ninja <ninja.jitrungpipat@gmail.com>
2026-08-28 05:28:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BUILDTRACK_CONFIG = {
   supabaseUrl: "https://qaijmbscufievjgdpyfg.supabase.co",
-  supabasePublishableKey: "sb_publishable_eq9dvVw60npCku7dMtvNbQ_dfO3qRXL"
+  supabasePublishableKey:"sb_publishable_eq9dvVw60npCku7dMtvNbQ_dfO3qRXL",
 };
